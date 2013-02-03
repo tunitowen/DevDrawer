f2ab016380bdd5e68a8f4aa7ee3837c69d1502bd
lithid <long.jeremie@gmail.com>
2013-02-03 11:49:05 -0500

    Add dialog when widget is clicked to allow user to choose activity
    Image: http://i.imgur.com/1kLpzth.png

--- src/com/owentech/DevDrawer/activities/ClickHandlingActivity.java
+++ src/com/owentech/DevDrawer/activities/ClickHandlingActivity.java
@@ -1,9 +1,23 @@
 package com.owentech.DevDrawer.activities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
 import com.owentech.DevDrawer.appwidget.DDWidgetProvider;
 
 /**
@@ -14,12 +28,14 @@ import com.owentech.DevDrawer.appwidget.DDWidgetProvider;
  * To change this template use File | Settings | File Templates.
  */
 public class ClickHandlingActivity extends Activity {
+	
+	CharSequence[] Packageitems = null;
 
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
 
-        String packageName = getIntent().getStringExtra(DDWidgetProvider.PACKAGE_STRING);
+        final String packageName = getIntent().getStringExtra(DDWidgetProvider.PACKAGE_STRING);
         boolean appDetails = getIntent().getBooleanExtra("appDetails", false);
 
         if (packageName != null) {
@@ -35,16 +51,59 @@ public class ClickHandlingActivity extends Activity {
             }
             else
             {
-				// Laumnch the app
-                Intent LaunchIntent = getApplicationContext().getPackageManager()
-                        .getLaunchIntentForPackage(packageName);
-				LaunchIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-				LaunchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(LaunchIntent);
+                
+                try {
+                	List<String> adapter = getActivityList(packageName);
+                	Packageitems = adapter.toArray(new CharSequence[adapter.size()]);
+				} catch (NameNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                
+                
+                
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.setTitle("Choose:");
+                alert.setCancelable(false);
+                alert.setSingleChoiceItems(Packageitems, -1, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int item){
+                    	Intent intent = new Intent();
+                    	intent.setComponent(new ComponentName(packageName, Packageitems[item].toString()));
+                    	intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    	intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    	startActivity(intent);
+                    	finish();
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        finish();
+                    }
+                });
+
+                AlertDialog ad = alert.create();
+                ad.show();
             }
         }
+    }
+    
+    private List<String> getActivityList(String packageName) throws NameNotFoundException {
 
-        finish();
+        PackageManager pm = this.getPackageManager();
+        
+        List<String> adapter = new ArrayList<String>();
+
+        PackageInfo info = pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+        ActivityInfo[] list = info.activities;
+        for (ActivityInfo string: list) {
+        	adapter.add(string.name.toString());
+        }
+        
+        return adapter;
     }
 
 }
