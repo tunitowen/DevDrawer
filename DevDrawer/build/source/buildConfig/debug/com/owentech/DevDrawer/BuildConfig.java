b128b397fda86bf0650afc60d673c223aa55adc6
Tony Owen <tony@owentech.co.uk>
2013-09-27 14:51:27 +0100

    Moved to Gradle

---
+++ DevDrawer/build/source/buildConfig/debug/com/owentech/DevDrawer/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.owentech.DevDrawer;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
