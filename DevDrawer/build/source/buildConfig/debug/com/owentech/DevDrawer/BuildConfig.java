605181dfa4f618602b617d3c2ba9af98be1226e3
Niek Haarman <haarman.niek@gmail.com>
2014-01-12 19:25:46 +0100

    Updated gitignore

--- DevDrawer/build/source/buildConfig/debug/com/owentech/DevDrawer/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.owentech.DevDrawer;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
